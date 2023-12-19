08874b5d58490a9d9182cfd9b26328a4511db807
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2023-12-19 11:25:34 -0800

    player sprite changes direction

--- assets/script/PLAYER.gd
+++ assets/script/PLAYER.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var speed = 5
 # Called when the node enters the scene tree for the first time.
-
+@onready var sprite = $Sprite2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(velocity):
 	velocity = Vector2.ZERO
@@ -16,7 +16,8 @@ func _process(velocity):
 		velocity.y += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		sprite.scale.x = 1
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	
+		sprite.scale.x = -1
 	position += velocity * speed
